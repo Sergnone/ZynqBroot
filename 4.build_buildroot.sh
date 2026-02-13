df5ede1b1ee690e8466842daddcc28fde10a7019
Sergey Frolov <sergeyfevg@gmail.com>
2026-02-13 15:37:36 +0300

    Adds board configs

---
+++ 4.build_buildroot.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+LINDIR=build/linux-865e73109750d465f06dee019396406a354eb9d2
+BIFDIR=platform/bifimage
+SCRDIR=platform/scripts
+UBOOTDIR=u-boot-xlnx
+
+cd buildroot
+make clean
+make distclean
+make br_def_defconfig BR2_EXTERNAL=$(pwd)/../board
+make O=$PWD ARCH=arm BR2_JLEVEL="6"
+
+cp $LINDIR/arch/arm/boot/uImage ../images/
+cp images/rootfs.tar ../images/
+cp images/rootfs.cpio.uboot ../images/
+cd ..
+cp $UBOOTDIR/arch/arm/dts/zynq-zc702.dtb images/
+sudo mv images/zynq-zc702.dtb images/devicetree.dtb
+#cp $BIFDIR/zynq-mini-port.dtb images/
+#sudo mv images/zynq-mini-port.dtb images/devicetree.dtb
+
+cd $SCRDIR
+mkimage -A arm -O linux -T script -C none -d boot.txt boot.scr
+cd ../..
+cp $SCRDIR/boot.scr images/
--- build_all.sh
+++ build_all.sh
@@ -6,3 +6,8 @@ source /tools/xilinx/2025.2/Vitis/settings64.sh
 ./1.prepare_dtb.sh
 ./2.build_uboot.sh
 ./3.build_bootbin.sh
+
+
+sudo make O=$PWD ARCH=arm BR2_JLEVEL="$(($(nproc) - 1))"
+
+file:///home/serg/Documents/Xilinx/ZynqBroot/platform/configs/zynq_mini_defconfig
